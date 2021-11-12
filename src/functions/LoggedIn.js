09d9a95530d45e5d3f8ea7221c07c7228d107294
pl0xi <puixdk@gmail.com>
2021-11-12 12:06:06 +0100

    Revert "css og styling"
    This reverts commit 3362fe90979f765a18041b2d655f99c7dd48dc82.

--- src/functions/LoggedIn.js
+++ src/functions/LoggedIn.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react"
-import './LoggedIn.css';
 
 export default function LoggedIn({facade}) {
     const [dataFromServer, setDataFromServer] = useState("Loading...")
@@ -17,17 +16,13 @@ export default function LoggedIn({facade}) {
     }, [facade])
    
     return (
-      <html>
-      <div class="topnav">
-      <a class="active" href="#memes">Memes</a>
+      <div>
+        <h2>Data Received from server</h2>
+        <h3>{dataFromServer}</h3>
+        <p>
+            {errorMsg}
+        </p>
       </div>
-      <h2>Data Received from server</h2>
-      <h3>{dataFromServer}</h3>
-      <p>
-      {errorMsg}
-      </p>
-      </html>
-      
     )
    
   }
